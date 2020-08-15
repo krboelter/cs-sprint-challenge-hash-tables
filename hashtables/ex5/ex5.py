427f481ec47c4ca56f66788d8c21b14f92b9b5f6
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-08-14 20:00:18 -0600

    progress on ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,23 @@
 # Your code here
 
 
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
     # Your code here
+    sep_dict = {}
+    for path in files:
+        separated = path.split("/")
+        if separated[-1] not in queries:
+            continue
+        else:
+            if separated[-1] not in sep_dict:
+                else:
+                sep_dict[separated[-1]] = [path]
+            else:
+                sep_dict[separated[-1]].append(path)
 
+    result = []
+    for v in sep_dict.values():
+        result.append(v)
     return result
 
 
@@ -15,7 +25,7 @@ if __name__ == "__main__":
     files = [
         '/bin/foo',
         '/bin/bar',
-        '/usr/bin/baz'
+        '/usr/bin/baz',
     ]
     queries = [
         "foo",
